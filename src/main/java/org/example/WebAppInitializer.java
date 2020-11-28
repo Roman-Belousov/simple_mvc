1de47a0874be7d41dbdc7dd34521e7a4bb5ed2b9
Roman-Belousov <5133463@gmail.com>
2020-11-28 23:08:46 +0300

    java-based config final

--- src/main/java/org/example/WebAppInitializer.java
+++ src/main/java/org/example/WebAppInitializer.java
@@ -1,22 +1,30 @@
 package org.example;
 
+import org.apache.log4j.Logger;
+import org.example.web.config.AppContextConfig;
+import org.example.web.config.WebContextConfig;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
-import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
 public class WebAppInitializer implements WebApplicationInitializer {
+
+    private Logger logger = Logger.getLogger(WebAppInitializer.class);
+
     @Override
     public void onStartup(javax.servlet.ServletContext servletContext) throws ServletException {
-        XmlWebApplicationContext appContext = new XmlWebApplicationContext();
-        appContext.setConfigLocation("classpath:app-config.xml");
-        servletContext.addListener(new ContextLoaderListener(appContext));
+        logger.info("loading app config");
+        AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+       appContext.register(AppContextConfig.class);
+      servletContext.addListener(new ContextLoaderListener(appContext));
 
-        XmlWebApplicationContext webContext = new XmlWebApplicationContext();
-        webContext.setConfigLocation("classpath:web-config.xml");
+        AnnotationConfigWebApplicationContext webContext = new AnnotationConfigWebApplicationContext();
+        webContext.register(WebContextConfig.class);
+        logger.info("loading web config");
 
         DispatcherServlet dispatcherServlet = new DispatcherServlet(webContext);
         ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", dispatcherServlet);
